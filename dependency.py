0f0f2fa5c2947a5733b013f0d11fec5a168a7e69
Saran-Kaanth <sarnknth16@gmail.com>
2022-03-16 20:33:36 +0530

    second

--- dependency.py
+++ dependency.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI,Depends, Query,Cookie
+from fastapi import FastAPI,Depends, Cookie,Header
+from fastapi.exceptions import HTTPException
+
 
 app=FastAPI()
 
@@ -32,6 +34,36 @@ def query_or_cookie_extractor(
     return q
 
 
-@app.get("/items/")
+@app.get("/items_1/",tags=["items"])
 async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
     return {"q_or_cookie": query_or_default}
+
+async def verify_token(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_key(x_key: str = Header(...)):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+
+@app.get("/items_except/", dependencies=[Depends(verify_token), Depends(verify_key)],tags=["items"],)
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
+
+# class MySuperContextManager:
+#     def __init__(self):
+#         self.db = DBSession()
+
+#     def __enter__(self):
+#         return self.db
+
+#     def __exit__(self, exc_type, exc_value, traceback):
+#         self.db.close()
+
+
+# async def get_db():
+#     with MySuperContextManager() as db:
+#         yield db
